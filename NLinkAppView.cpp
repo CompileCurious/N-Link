9dbd675a710fcf1db2c5631deca1dfc196ffa729
CompileCurious <samueljsharp@protonmail.com>
2025-08-06 21:53:12 +0100

    Create NLinkAppView.cpp

---
+++ NLinkAppView.cpp
@@ -0,0 +1,9 @@
+#include "NLinkAppView.h"
+#include "ChatEngine.h"
+#include <akntexteditor.h>
+#include <akntextquerydialog.h>
+
+void CNLinkAppView::ConstructL() {
+    iChatEngine = CChatEngine::NewL();
+
+    iEditor =[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/kristtopher/CLionProjects/tree/a689c1b7163644c05413e6b8b519d42cd0449446/TEA%2FTEAENCRYPTOR.cpp?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "1")[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/scnu-sloth/hsctf-2020-freshmen/tree/7e960dc1901cdc879ce90829d9d32fec2daf9e34/Reverse-Deliver_TEA_to_dalao%2Ftqltql_exp.c?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "2")[43dcd9a7-70db-4a1f-b0ae-981daa162054](https://github.com/Matixx00/Projekt-SYCY/tree/dd0aea405a87850e862fb8b0cf50c178e3f62775/C_model_ref%2Ftea_by_wheeler_needham.c?citationMarker=43dcd9a7-70db-4a1f-b0ae-981daa162054 "3")
